ed5e3d44e92bee04e7f05de32fb21aa30d2e0d01
ramdos0207 <dev@ramdos.net>
2023-12-30 19:20:34 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+import buttonsList from './components/buttonsList.vue';
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth();
+const date = now.getDate();
+const selectedyear = ref<number>(year);
+const selectedmonth = ref<number>(month);
+const selecteddate = ref<number>(date);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <buttonsList :begin="year-2" :end="year+3" :current="year" @update="(n) => selectedyear = n" />
+  <buttonsList :begin="1" :end="13" :current="month+1" @update="(n) => selectedmonth = n" />
+  <buttonsList :begin="1" :end="32" :current="date" @update="(n) => selecteddate = n" />
+  {{ selectedyear }}-{{ selectedmonth+1 }}-{{ selecteddate }}
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/buttonsList.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+const props = defineProps<{
+  begin: number
+  end: number
+  current: number
+}>();
+const current = ref(props.current);
+const arr = [...Array(props.end-props.begin)].map((_, i) => i+props.begin) //=> [ 0, 1, 2, 3, 4 ]
+</script>
+
+<template>
+  <div>
+  <div class="ct" v-for="item in arr" :key="item">
+    <button class="btn highlight" @click="current=item;$emit('update',current)" v-if="item==props.current">{{ item }}</button>
+    <button class="btn" @click="current=item;$emit('update',current)" v-else>{{ item }}</button>
+  </div>
+  <input class="ipt" v-model="current" @input="$emit('update',current)">
+</div>
+</template>
+
+<style scoped>
+.ct{
+  float: left;
+}
+.highlight{
+  background-color: #fcc;
+}
+.btn{
+  width: 50px;
+  height: 30px;
+  border: 1px solid #000;
+  border-radius: 5px;
+  margin: 2px;
+}
+.ipt{
+  width: 60px;
+  height: 27px;
+  border: 1px solid #000;
+  border-radius: 5px;
+  margin: 2px;
+}
+</style>
